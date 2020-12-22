3755467f0d925f0ef31a9edd12d0092090b54bb7
Eddine Omar <eddine.farid@hotmail.fr>
2020-12-22 19:09:27 +0100

    add a comment

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from functions import getPriceFeed, getAllPriceFeed, orfeed_i
 import pprint
 from web3 import Web3
 import web3
-token = "DAI"
 
+# token = "DAI"
 # getPriceFeed("DAI")
 pprint.pprint(getAllPriceFeed())
