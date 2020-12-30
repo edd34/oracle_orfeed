a23d68d58267ea36bc00be370171304132ade7d9
Eddine Omar <eddine.farid@hotmail.fr>
2020-12-30 19:07:09 +0100

    update functions.py

--- functions.py
+++ functions.py
@@ -1,27 +1,22 @@
 import os
 import time
 import pprint
-from data import init_dict_token_dex, orfeed_list_providers
-from orfeed import Orfeed, Registry
+from data import init_dict_token_dex, orfeed_list_providers, init_dict_token_token_dex
+from orfeed import Orfeed
 from dotenv import load_dotenv
 load_dotenv()
 from web3 import Web3
 
-orfeed_i = Orfeed(os.getenv("NETWORK"), os.getenv("INFURA_PROJECT_ID"))
-registry_i = Registry(os.getenv("NETWORK"), os.getenv("INFURA_PROJECT_ID"))
-dict_token_dex = init_dict_token_dex()
+def getEthToTokenPriceFeed(orfeed_i, threshold = 0, verbose = False):
+    result = {}
+    dict_token_dex = init_dict_token_dex()
 
-def getAllPriceFeed(threshold = 0):
-    result = []
-    
     for token in dict_token_dex:
-        a = time.perf_counter()
         tmp = {
             token: {}
         }
-        
         for provider in dict_token_dex[token]:
-            res = orfeed_i.getExchangeRate("ETH", token, provider, 1)
+            res = orfeed_i.getExchangeRateEthToToken(token, provider)
             if res > 0:
                 tmp[token][provider] = res
                 # print(token + "("+ provider+")" + " = " + str(res))
@@ -31,12 +26,12 @@ def getAllPriceFeed(threshold = 0):
 
         key_max = max(tmp[token].keys(), key=(lambda k:tmp[token][k]))
         key_min = min(tmp[token].keys(), key=(lambda k:tmp[token][k]))
-        
+
         if key_min == key_max:
             continue
 
         verdict = {
-            "token" : token,
+            # "token_address": orfeed_i.getTokenAddress(token),
             "buy" : {
                 "provider" : key_min,
                 "price" : tmp[token][key_min]
@@ -51,43 +46,34 @@ def getAllPriceFeed(threshold = 0):
             }
         }
         if float(verdict["res"]["%"]) > threshold:
-            result.append(verdict)
+            result[token] = verdict
+            if verbose:
+                pprint.pprint(verdict)
+
     return result
 
+def getTokenToTokenPriceFeed(orfeed_i, threshold = 0):
+    result = []
+    dict_token_dex = init_dict_token_token_dex()
+    for pair in dict_token_dex:
+        tmp_res = {}
+        for provider in ["UNISWAPBYSYMBOLV2", "KYBERBYSYMBOLV1"]:
+            res = getTokenToTokenPrice(orfeed_i, pair["tokenSrc"], pair["tokenDst"], provider)
 
-def getPriceFeed(token):
-    tmp = {
-        token: {}
+            if tmp_res.get(res["tokenPair"], None) == None:
+                tmp_res[res["tokenPair"]] = {}
+            tmp_res[res["tokenPair"]]["provider"] = provider
+            tmp_res[res["tokenPair"]]["price"] = res["price"]
+            pprint.pprint(tmp_res)
+
+
+def getTokenToTokenPrice(orfeed_i, tokenSrc, tokenDst, provider):
+    res = orfeed_i.getExchangeRateNormalized(tokenSrc, tokenDst, provider)
+
+    return {
+        "tokenSrc" : tokenSrc,
+        "tokenDst" : tokenDst,
+        "tokenPair" : tokenSrc + '-' + tokenDst,
+        "provider" : provider,
+        "price" : res
     }
-
-    for provider in orfeed_list_providers:
-        res = orfeed_i.getExchangeRate("ETH", token, provider, 1)
-        if res > 0:
-            tmp[token][provider] = res
-
-    if not tmp[token]:
-        return {}
-
-    key_max = max(tmp[token].keys(), key=(lambda k:tmp[token][k]))
-    key_min = min(tmp[token].keys(), key=(lambda k:tmp[token][k]))
-    
-    if key_min == key_max:
-        return {}
-
-    verdict = {
-        token: {
-            "buy" : {
-                "provider" : key_min,
-                "at" : tmp[token][key_min]
-            } ,
-            "sell" : {
-                "provider" : key_max,
-                "at" : tmp[token][key_max]
-            },
-            "res" : {
-                "%" : (tmp[token][key_max] - tmp[token][key_min]) * 100 /tmp[token][key_min] ,
-                "spread" : tmp[token][key_max] - tmp[token][key_min]
-            }
-        }
-    }
-    return verdict
