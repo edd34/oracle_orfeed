c3941c07a208ab9fa2737e5459876d857750a3d9
Eddine Omar <eddine.farid@hotmail.fr>
2020-12-31 02:40:30 +0100

    fix arb detection function

--- functions.py
+++ functions.py
@@ -5,7 +5,9 @@ from data import init_dict_token_dex, orfeed_list_providers, init_dict_token_tok
 from orfeed import Orfeed
 from dotenv import load_dotenv
 load_dotenv()
+import web3
 from web3 import Web3
+from tqdm import tqdm
 
 def getEthToTokenPriceFeed(orfeed_i, threshold = 0, verbose = False):
     result = {}
@@ -52,23 +54,60 @@ def getEthToTokenPriceFeed(orfeed_i, threshold = 0, verbose = False):
 
     return result
 
-def getTokenToTokenPriceFeed(orfeed_i, threshold = 0):
-    result = []
-    dict_token_dex = init_dict_token_token_dex()
-    for pair in dict_token_dex:
+def getTokenToTokenPriceFeed(orfeed_i, threshold = 0, verbose = False):
+    result = {}
+    dict_token_dex = init_dict_token_dex()
+    for token in tqdm(dict_token_dex, desc="finding arbitrage", total=len(dict_token_dex)):
         tmp_res = {}
         for provider in ["UNISWAPBYSYMBOLV2", "KYBERBYSYMBOLV1"]:
-            res = getTokenToTokenPrice(orfeed_i, pair["tokenSrc"], pair["tokenDst"], provider)
+            buy = getTokenToTokenPrice(orfeed_i, "WETH", token, provider, amount=orfeed_i.w3.toWei('1', 'ether'))
+            sell = getTokenToTokenPrice(orfeed_i, token, "WETH", provider, amount=buy["price"])
+            if not(buy["price"] > 0 and sell["price"] > 0):
+                continue
+            
+            tmp_res[provider] = {
+                "buy_price_wei": buy["price"]/1e18,
+                "sell_price_wei": sell["price"]*buy["price"]/(1e18*1e18),
+            }
 
-            if tmp_res.get(res["tokenPair"], None) == None:
-                tmp_res[res["tokenPair"]] = {}
-            tmp_res[res["tokenPair"]]["provider"] = provider
-            tmp_res[res["tokenPair"]]["price"] = res["price"]
-            pprint.pprint(tmp_res)
+        if len(tmp_res.keys()) != 2:
+            continue
+
+        path = {
+            "U->K": (tmp_res['KYBERBYSYMBOLV1']['sell_price_wei'] - tmp_res['UNISWAPBYSYMBOLV2']['buy_price_wei'])/tmp_res['UNISWAPBYSYMBOLV2']['buy_price_wei'] * 100,
+            "K->U": (tmp_res['UNISWAPBYSYMBOLV2']['sell_price_wei'] - tmp_res['KYBERBYSYMBOLV1']['buy_price_wei'])/tmp_res['KYBERBYSYMBOLV1']['buy_price_wei'] * 100
+        }
+        if path["U->K"] > path["K->U"] and path["U->K"] > 0: # buy at uniswap and sell at kyber
+            result[token] = {
+                "buy_at": "UNISWAPBYSYMBOLV2",
+                "buy_price": tmp_res["UNISWAPBYSYMBOLV2"]["buy_price_wei"],
+                "sell_at": "KYBERBYSYMBOLV1",
+                "sell_price": tmp_res["KYBERBYSYMBOLV1"]["sell_price_wei"],
+                "%": path["U->K"]
+            }
+        elif path["K->U"] > path["U->K"] and path["K->U"] > 0: # buy at kyber and sell at uniswap
+            result[token] = {
+                "sell_at": "UNISWAPBYSYMBOLV2",
+                "sell_price": tmp_res["UNISWAPBYSYMBOLV2"]["sell_price_wei"],
+                "buy_at": "KYBERBYSYMBOLV1",
+                "buy_price": tmp_res["KYBERBYSYMBOLV1"]["buy_price_wei"],
+                "%": path["K->U"]
+            }
+        else:
+            continue
+
+        if verbose:
+            print(""+ token + ' : ' + result[token]["buy_at"] + " -> " + result[token]["sell_at"] + ' : ' + str(result[token]["%"]))
+            # pprint.pprint({token: result[token]})
+            print("")
+    return result
 
 
-def getTokenToTokenPrice(orfeed_i, tokenSrc, tokenDst, provider):
-    res = orfeed_i.getExchangeRateNormalized(tokenSrc, tokenDst, provider)
+def getTokenToTokenPrice(orfeed_i, tokenSrc, tokenDst, provider, amount=1, normalized=False):
+    if normalized == True:
+        res = orfeed_i.getExchangeRateNormalized(tokenSrc, tokenDst, provider)
+    else:
+        res = orfeed_i.getExchangeRate(tokenSrc, tokenDst, provider, amount)
 
     return {
         "tokenSrc" : tokenSrc,
