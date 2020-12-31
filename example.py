da7dcfff5c98961f81cf7dff9f59af71bde33da1
Eddine Omar <eddine.farid@hotmail.fr>
2020-12-31 02:41:00 +0100

    update example.py

--- example.py
+++ example.py
@@ -6,10 +6,16 @@ import os
 from orfeed import Orfeed
 from data import init_dict_token_dex, orfeed_list_providers
 import time
+from tqdm import tqdm
 
 w3 = Web3(Web3.HTTPProvider('https://'+os.getenv("NETWORK")+'.infura.io/v3/'+os.getenv("INFURA_PROJECT_ID")))
+# w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
 orfeed_i = Orfeed(w3)
-res = getEthToTokenPriceFeed(orfeed_i, verbose = True)
-sorted_list = sorted(res.items(), key=lambda x: x[1]["res"]["%"], reverse=True)
-top3 = [sorted_list[i] for i in range(3)]
-pprint.pprint(top3)
+while True:
+    res = getTokenToTokenPriceFeed(orfeed_i, verbose=True)
+    sorted_list = sorted(res.items(), key=lambda x: x[1]["%"], reverse=True)
+    os.system('clear')
+    pprint.pprint(sorted_list)
+    cooldown = 20
+    for i in tqdm(range(cooldown), desc="cooldown, waiting for new block"):
+        time.sleep(1)
