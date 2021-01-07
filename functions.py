802ec6c57e207c23103cc00e3adbdf8d7a8492c8
Eddine Omar <eddine.farid@hotmail.fr>
2021-01-07 01:13:25 +0100

    add function simple_getTokenToTokenPrice

--- functions.py
+++ functions.py
@@ -58,6 +58,46 @@ def getTokenToTokenPriceFeed(orfeed_i, threshold = 0, verbose = False):
             print("")
     return result
 
+def simple_getTokenToTokenPrice(orfeed_i, token):
+    result = {}
+    providers_list = ["UNISWAPBYSYMBOLV2", "KYBERBYSYMBOLV1"]
+    tmp_res = {}
+    for provider in providers_list:
+        buy = getTokenToTokenPrice(orfeed_i, "ETH", token, provider, amount=orfeed_i.w3.toWei('1', 'ether'))
+        sell = getTokenToTokenPrice(orfeed_i, token, "ETH", provider, amount=buy["price"])
+        if buy["price"] > 0 and sell["price"] > 0:
+            tmp_res[provider] = {
+                "buy_price_wei": buy["price"]/1e18,
+                "sell_price_wei": sell["price"]*buy["price"]/(1e18*1e18),
+            }
+        else:
+            return -1
+
+    path = {
+        "one": (tmp_res[providers_list[1]]['sell_price_wei'] - tmp_res[providers_list[0]]['buy_price_wei'])/tmp_res[providers_list[0]]['buy_price_wei'] * 100,
+        "two": (tmp_res[providers_list[0]]['sell_price_wei'] - tmp_res[providers_list[1]]['buy_price_wei'])/tmp_res[providers_list[1]]['buy_price_wei'] * 100
+    }
+
+    if path["one"] > path["two"] and path["one"] > 0: # buy at uniswap and sell at kyber
+        result = {
+            "buy_at": providers_list[0],
+            "buy_price": tmp_res[providers_list[0]]["buy_price_wei"],
+            "sell_at": providers_list[1],
+            "sell_price": tmp_res[providers_list[1]]["sell_price_wei"],
+            "%": path["one"]
+        }
+    elif path["two"] > path["one"] and path["two"] > 0: # buy at kyber and sell at uniswap
+        result = {
+            "sell_at": providers_list[0],
+            "sell_price": tmp_res[providers_list[0]]["sell_price_wei"],
+            "buy_at": providers_list[1],
+            "buy_price": tmp_res[providers_list[1]]["buy_price_wei"],
+            "%": path["two"]
+        }
+    else:
+        result = -1
+
+    return result
 
 def getTokenToTokenPrice(orfeed_i, tokenSrc, tokenDst, provider, amount=1, normalized=False):
     if normalized == True:
